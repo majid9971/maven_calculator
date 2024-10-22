d60eb81c3d4ea652c5da0b075879cb5258fa40ff
Majid Mohammednisar Siddiqui <majids@cybage.com>
2024-10-22 15:40:35 +0530

    first commit

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,25 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator
+{
+    // public static void main( String[] args )
+    // {
+    //     System.out.println( "Hello World!" );
+    // }
+   public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
